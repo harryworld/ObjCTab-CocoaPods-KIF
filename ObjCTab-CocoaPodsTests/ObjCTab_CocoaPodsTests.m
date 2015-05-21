1245373bd56126a7faa46773cfaed1182b5bb8c7
STAY REAL <stayreal@STAYs-MacBook-Air.local>
2015-05-21 18:28:53 +0800

    Modify test to KIF

--- ObjCTab-CocoaPodsTests/ObjCTab_CocoaPodsTests.m
+++ ObjCTab-CocoaPodsTests/ObjCTab_CocoaPodsTests.m
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import <KIF/KIF.h>
 
 @interface ObjCTab_CocoaPodsTests : XCTestCase
 
@@ -15,25 +16,19 @@
 
 @implementation ObjCTab_CocoaPodsTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
 - (void)testExample {
     // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+    [tester waitForViewWithAccessibilityLabel:@"First View"];
 }
 
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
+        [tester tapViewWithAccessibilityLabel:@"Second"];
+        [tester waitForViewWithAccessibilityLabel:@"Second View"];
+        [tester tapViewWithAccessibilityLabel:@"First"];
+        [tester waitForViewWithAccessibilityLabel:@"First View"];
     }];
 }
 
